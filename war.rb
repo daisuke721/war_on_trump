feef2a8fe10b74573cd3d252c4beb38ae87d5e3a
daisuke721 <d.magome.07@gmail.com>
2024-10-20 18:13:55 +0900

    Playerクラスの作成

--- war.rb
+++ war.rb
@@ -1,8 +1,11 @@
 puts '戦争を開始します'
-class CARD
+class Card
   def initialize
     # カードのランクを定義
-    @card_ranks = {'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 11, '10' => 10, '9' => 9, '8' => 8, '7' => 7, '6' => 6, '5' => 5, '4' => 4, '3' => 3, '2' => 2}
+    @card_ranks = {
+      'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 11, '10' => 10, '9' => 9,
+      '8' => 8, '7' => 7, '6' => 6, '5' => 5, '4' => 4, '3' => 3, '2' => 2,
+    }
   end
 
   # デッキを作成するメソッドを作成
@@ -35,24 +38,69 @@ class CARD
     rank2 = card2[0]
 
     # 条件分岐を使ってカードの強さを比較する
+    # カード1が強い場合
     if @card_ranks[rank1] > @card_ranks[rank2]
-      puts "カード1 (#{card1[1]}の#{rank1}) の勝利"
+      card1
+    # カード2が強い場合
     elsif @card_ranks[rank1] < @card_ranks[rank2]
-      puts "カード2 (#{card2[1]}の#{rank2}) の勝利"
+      card2
+    # 引き分けの場合
     else
-      '引き分け'
+      nil
     end
   end
 end
 
-# CARDクラスのインスタンスを生成
-created_card = CARD.new
+class Player
+  # プレイヤーの名前と手札を作成
+  def initialize(name, hand)
+    @name = name
+    @hand = hand
+  end
 
-# deck_creationでデッキを作成し、それを変数に渡す
-deck = created_card.deck_creation
+  # 名前を取得するメソッド
+  def get_name
+    @name
+  end
 
-# カードの強さを比較
-hands = created_card.deck_deal(deck)
-card1 = hands[0][0]
-card2 = hands[1][0]
-created_card.card_strength(card1, card2)
+  # 手札の枚数を把握するメソッド
+  def count_card
+    # lengthメソッドを使い配列を展開
+    @hand.length
+  end
+
+  # 手札からカードを出すメソッド
+  def play_card
+    # 最初のカードを取り出す
+    @hand.shift
+  end
+
+  # 勝ったらカードをもらうメソッド
+  def get_cards(cards)
+    # concatメソッドを使って配列の結合
+    @hand.concat(cards)
+  end
+
+  # 手札を持っているかどうかのメソッド
+  def has_cards?
+    # lengthメソッドを使い配列を展開
+    @hand.length > 0
+  end
+end
+
+player = Player.new("Alice", ["3H", "5D", "10S"])
+
+# 名前を取得
+puts player.get_name
+
+# カードを数える
+puts player.count_card
+
+# カードをプレイする
+puts player.play_card
+
+# カードを追加する
+puts player.get_cards(["8C", "9D"])
+
+# カードが残っているか確認
+puts player.has_cards?
